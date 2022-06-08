8b51e706d195dc587997eb497d42ea0be263bd0e
Percy Ma <kecrily@gmail.com>
2022-06-08 17:31:19 +0800

    feat(ts): more specific indent rules

--- src/ts.js
+++ src/ts.js
@@ -17,9 +17,10 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
       ],
       rules: {
+        'import/named': 'off',
+
+        // override
         'no-useless-constructor': 'off',
-        'indent': 'off',
-        '@typescript-eslint/indent': ['error', 2],
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
         'no-redeclare': 'off',
@@ -32,10 +33,72 @@ module.exports = {
         '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
         'object-curly-spacing': 'off',
         '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+        'indent': 'off',
+        '@typescript-eslint/indent': [
+          'error', 2, {
+            SwitchCase: 1,
+            VariableDeclarator: 1,
+            outerIIFEBody: 1,
+            MemberExpression: 1,
+            FunctionDeclaration: { parameters: 1, body: 1 },
+            FunctionExpression: { parameters: 1, body: 1 },
+            CallExpression: { arguments: 1 },
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false,
+            ignoreComments: false,
+            ignoredNodes: [
+              'TemplateLiteral *',
+              'JSXElement',
+              'JSXElement > *',
+              'JSXAttribute',
+              'JSXIdentifier',
+              'JSXNamespacedName',
+              'JSXMemberExpression',
+              'JSXSpreadAttribute',
+              'JSXExpressionContainer',
+              'JSXOpeningElement',
+              'JSXClosingElement',
+              'JSXFragment',
+              'JSXOpeningFragment',
+              'JSXClosingFragment',
+              'JSXText',
+              'JSXEmptyExpression',
+              'JSXSpreadChild',
+              'TSTypeParameterInstantiation',
+            ],
+            offsetTernaryExpressions: true,
+          },
+        ],
+
+        // TypeScript
+        '@typescript-eslint/semi': ['error', 'never'],
+        '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+        '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
+        '@typescript-eslint/type-annotation-spacing': ['error', {}],
+        '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+        '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+        '@typescript-eslint/prefer-ts-expect-error': 'error',
+
+        // off
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/parameter-properties': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-namespace': 'off',
       },
     },
     {
-      // Code blocks in markdown file
+    // code block
       files: ['**/*.md/*.ts'],
       rules: {
         '@typescript-eslint/no-redeclare': 'off',
@@ -45,31 +108,4 @@ module.exports = {
       },
     },
   ],
-  rules: {
-    'import/named': 'off',
-
-    // TypeScript
-    '@typescript-eslint/semi': ['error', 'never'],
-    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-    '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
-    '@typescript-eslint/type-annotation-spacing': ['error', {}],
-    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
-    '@typescript-eslint/prefer-ts-expect-error': 'error',
-
-    // off
-    '@typescript-eslint/naming-convention': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/parameter-properties': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-  },
 }
