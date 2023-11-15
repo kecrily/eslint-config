0f74a3ebddf9b95171758213877b8358a525d64f
Percy Ma <kecrily@gmail.com>
2023-11-15 22:08:31 +0800

    fix(json): multiple of the same rules

--- src/config/json.ts
+++ src/config/json.ts
@@ -1,4 +1,4 @@
-import { GLOB_JSONC, GLOB_JSONC5 } from '../globs'
+import { GLOB_JSONC5 } from '../globs'
 import { parserJsonc } from '../parsers'
 import { pluginJsonc } from '../plugins'
 import type { ConfigItem } from '../types'
@@ -13,7 +13,6 @@ export const json: ConfigItem[] = [
       'no-unused-expressions': 'off',
       'no-unused-vars': 'off',
 
-      'jsonc/comma-dangle': 'error',
       'jsonc/no-bigint-literals': 'error',
       'jsonc/no-binary-expression': 'error',
       'jsonc/no-binary-numeric-literals': 'error',
@@ -41,62 +40,29 @@ export const json: ConfigItem[] = [
       'jsonc/space-unary-ops': 'error',
       'jsonc/valid-json-number': 'error',
       'jsonc/vue-custom-block/no-parsing-error': 'error',
-      'jsonc/no-irregular-whitespace': 'error'
+      'jsonc/no-irregular-whitespace': 'error',
+
+      'jsonc/array-bracket-spacing': ['error', 'never'],
+      'jsonc/comma-style': ['error', 'last'],
+      'jsonc/comma-dangle': ['error', 'never'],
+      'jsonc/indent': ['error', 2],
+      'jsonc/key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'jsonc/object-curly-newline': ['error', { consistent: true, multiline: true }],
+      'jsonc/object-curly-spacing': ['error', 'always'],
+      'jsonc/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }]
     }
   },
-  {
-    files: [GLOB_JSONC],
-    rules: {
-      'jsonc/no-bigint-literals': 'error',
-      'jsonc/no-binary-expression': 'error',
-      'jsonc/no-binary-numeric-literals': 'error',
-      'jsonc/no-dupe-keys': 'error',
-      'jsonc/no-escape-sequence-in-identifier': 'error',
-      'jsonc/no-floating-decimal': 'error',
-      'jsonc/no-hexadecimal-numeric-literals': 'error',
-      'jsonc/no-infinity': 'error',
-      'jsonc/no-multi-str': 'error',
-      'jsonc/no-nan': 'error',
-      'jsonc/no-number-props': 'error',
-      'jsonc/no-numeric-separators': 'error',
-      'jsonc/no-octal-numeric-literals': 'error',
-      'jsonc/no-octal': 'error',
-      'jsonc/no-parenthesized': 'error',
-      'jsonc/no-plus-sign': 'error',
-      'jsonc/no-regexp-literals': 'error',
-      'jsonc/no-sparse-arrays': 'error',
-      'jsonc/no-template-literals': 'error',
-      'jsonc/no-undefined-value': 'error',
-      'jsonc/no-unicode-codepoint-escapes': 'error',
-      'jsonc/no-useless-escape': 'error',
-      'jsonc/quote-props': 'error',
-      'jsonc/quotes': 'error',
-      'jsonc/space-unary-ops': 'error',
-      'jsonc/valid-json-number': 'error',
-      'jsonc/vue-custom-block/no-parsing-error': 'error'
-    }
-  },
+
   {
     files: ['*.json5'],
     rules: {
-      'jsonc/no-bigint-literals': 'error',
-      'jsonc/no-binary-expression': 'error',
-      'jsonc/no-binary-numeric-literals': 'error',
-      'jsonc/no-dupe-keys': 'error',
-      'jsonc/no-escape-sequence-in-identifier': 'error',
-      'jsonc/no-number-props': 'error',
-      'jsonc/no-numeric-separators': 'error',
-      'jsonc/no-octal-numeric-literals': 'error',
-      'jsonc/no-octal': 'error',
-      'jsonc/no-parenthesized': 'error',
-      'jsonc/no-regexp-literals': 'error',
-      'jsonc/no-sparse-arrays': 'error',
-      'jsonc/no-template-literals': 'error',
-      'jsonc/no-undefined-value': 'error',
-      'jsonc/no-unicode-codepoint-escapes': 'error',
-      'jsonc/no-useless-escape': 'error',
-      'jsonc/space-unary-ops': 'error',
-      'jsonc/vue-custom-block/no-parsing-error': 'error'
+      'jsonc/no-hexadecimal-numeric-literals': 'off',
+      'jsonc/no-infinity': 'off',
+      'jsonc/no-nan': 'off',
+      'jsonc/no-plus-sign': 'off',
+      'jsonc/quote-props': 'off',
+      'jsonc/quotes': 'off',
+      'jsonc/valid-json-number': 'off'
     }
   },
   {
